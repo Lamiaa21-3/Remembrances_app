f44ba5c782262a29367b3df9113938c9b9a716ef
Lamiaa21-3 <lamiaagamal213@gmail.com>
2024-10-07 11:36:35 +0200

    neeeeeeeeeeeeeeeeeeeeeeeeew

--- lib/features/home/presentation/widgets/home_page_body.dart
+++ lib/features/home/presentation/widgets/home_page_body.dart
@@ -5,9 +5,9 @@ import 'package:qoraan_app/cora/utilites/styles_app.dart';
 import 'package:qoraan_app/cora/utilites/widgets/custom_arabic_text.dart';
 
 import '../../../../generated/l10n.dart';
-import 'home_page_custom_row_words.dart';
+
 import 'home_page_custom_timing_container.dart';
-import 'home_page_list_view_container.dart';
+
 
 class HomePageBody extends StatelessWidget {
   const HomePageBody({super.key});
@@ -18,11 +18,14 @@ class HomePageBody extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          CustomArabicText(
-            text: S.of(context).hDate,
-            fontSize: 15,
-            fontWeight: FontWeight.w600,
-            color: ColorsApp.extraGreen,
+          Padding(
+            padding: const EdgeInsets.only(right: 20.0),
+            child: CustomArabicText(
+              text: S.of(context).hDate,
+              fontSize: 15,
+              fontWeight: FontWeight.w600,
+              color: ColorsApp.extraGreen,
+            ),
           ),
 
           const SizedBox(
@@ -30,58 +33,19 @@ class HomePageBody extends StatelessWidget {
           ),
           Image.asset('assets/images/Prayer.png'),
           const SizedBox(
-            height: 10,
+            height: 9,
           ),
           const HomePageCustomTimingContainer(),
           const SizedBox(
             height: 8,
           ),
 
-          CustomArabicText(
-            text: S.of(context).religious,
-            fontSize: 16,
-            fontWeight: FontWeight.w700,
-            color: ColorsApp.black,
 
-          ),
-          const SizedBox(
-            height: 6,
-          ),
 
-          const HomePageListViewContainer(),
-          const HomePageCustomRowWords(),
-          const HomePageListViewViedo(),
         ],
       ),
     );
   }
 }
 
-class HomePageListViewViedo extends StatelessWidget {
-  const HomePageListViewViedo({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 160,
-      child: ListView.separated(
-        scrollDirection: Axis.horizontal,
-        itemCount: 10,
-        itemBuilder: (BuildContext context, int index) {
-          return Stack(children: [
-            Image.asset('assets/images/Group 465.png'),
-            Positioned(
-                top: 40,
-                right: 113,
-                child: Image.asset('assets/images/run.png')),
-          ]);
-        },
-        separatorBuilder: (BuildContext context, int index) {
-          return const SizedBox(
-            width: 14,
-          );
-        },
-      ),
-    );
-  }
-}
--- lib/features/home/presentation/widgets/home_page_custom_timing_container.dart
+++ lib/features/home/presentation/widgets/home_page_custom_timing_container.dart
@@ -7,48 +7,51 @@ class HomePageCustomTimingContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: MediaQuery.of(context).size.height * .2,
-      width: MediaQuery.of(context).size.width * 2.4,
-      decoration: BoxDecoration(
-        color: ColorsApp.extraGreen,
-        borderRadius: BorderRadius.circular(17),
-      ),
-      child: Row(
-        children: [
-          Image.asset('assets/images/qotas.png',height: 158,fit: BoxFit.fill,),
-          const Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'الظهر',
-                style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.white),
-              ),
-              Text(
-                '11:45 ',
-                style: TextStyle(
-                    fontSize: 40,
-                    fontWeight: FontWeight.w600,
-                    color: Colors.white),
-              ),
-      
-              Padding(
-                padding: EdgeInsets.only(right: 99.0),
-                child: Text(
-                  'م ',
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Container(
+        height: MediaQuery.of(context).size.height * .2,
+        width: MediaQuery.of(context).size.width * 2.4,
+        decoration: BoxDecoration(
+          color: ColorsApp.extraGreen,
+          borderRadius: BorderRadius.circular(17),
+        ),
+        child: Row(
+          children: [
+            Image.asset('assets/images/qotas.png',height: 160,fit: BoxFit.fill,),
+            const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'الظهر',
                   style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.white,  ),
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white),
                 ),
-              ),
-            ],
-          ),
-      
-        ],
+                Text(
+                  '11:45 ',
+                  style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white),
+                ),
+
+                Padding(
+                  padding: EdgeInsets.only(right: 99.0),
+                  child: Text(
+                    'م ',
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white,  ),
+                  ),
+                ),
+              ],
+            ),
+
+          ],
+        ),
       ),
     );
   }
